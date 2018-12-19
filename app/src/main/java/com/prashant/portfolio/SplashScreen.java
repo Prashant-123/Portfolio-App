c8c74721f5939a35403db04a3b5533b10bfe589b
Prashant-123 <pk021998@gmail.com>
2018-12-19 17:16:02 +0530

    Initial commit and Splash Screen

---
+++ app/src/main/java/com/prashant/portfolio/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.prashant.portfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @BindView(R.id.splash_text)
+    TextView splashText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ButterKnife.bind(this);
+
+        splashText.setLetterSpacing((float) 0.1);
+
+    }
+}
